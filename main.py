5b1e13a70b53b8b50013c399e42246369c5042ee
zFPNC <nicolasguimafpnc@gmail.com>
2024-12-13 17:53:50 -0300

    Add Titles, descriptions, images, and links to the Webpage (data.csv) photo.gif

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.gif", width=300)
 
 with col2:
     st.title("Hello, My name is Nicolas")
@@ -18,3 +19,20 @@ content2 = """
 Below you can find some of the app I have built in Python. Feel free to contact me!
 """
 st.write(content2)
+
+col3 , col4 = st.columns(2)
+
+df = pd.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], caption=row["title"])
+        st.write(f"[Source Code]({row["url"]})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], caption=row["title"])
+        st.write(f"[Source Code]({row["url"]})")
