42148037825ddb6e3bec3a0dfa02bd9aba9eb83f
zFPNC <nicolasguimafpnc@gmail.com>
2024-12-16 15:55:34 -0300

    Connection between email and portfolio

--- send_email.py
+++ send_email.py
@@ -1,20 +1,16 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "ng4324757@gmail.com"
-passwords = "kmht bwuc lzak nxqn"
+    username = "ng4324757@gmail.com"
+    passwords = "kmht bwuc lzak nxqn"
 
-receiver = "ng4324757@gmail.com"
-context = ssl.create_default_context()
+    receiver = "ng4324757@gmail.com"
+    context = ssl.create_default_context()
 
-message = """
-Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, passwords)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, passwords)
+        server.sendmail(username, receiver, message)
+    print('E-mail send successfully')
